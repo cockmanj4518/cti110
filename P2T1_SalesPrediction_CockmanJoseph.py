19a80a67ac72089882f1a3c1f729117eafb8a6bc
cockmanj4518 <68261135+cockmanj4518@users.noreply.github.com>
2020-07-13 23:14:43 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_CockmanJoseph.py
@@ -0,0 +1,13 @@
+#A brief description of the project
+#06-24-2020
+#CTI-110 P2T1 - Sales Prediction
+#Joseph Cockman
+#
+#Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+
+#Display the profit
+print('The Profit is $', format(profit, ',.2f'))
